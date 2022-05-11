740068b4ba6fc50cf001a4d08884805f8b2d12cd
Eduard Malyshenko <ed54rus@mail.ru>
2022-05-11 18:30:32 +0700

    разделено на компоненты

---
+++ ReactApp/react-app-01/src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+	return (
+		<header className='header'>
+			<img
+				className='img'
+				src='https://avatars.mds.yandex.net/i?id=c3bd8df4b20931c89e97017092eb0eb8-4073140-images-thumbs&n=13&exp=1'
+			/>
+		</header>
+	);
+};
+
+export default Header;
---
+++ ReactApp/react-app-01/src/components/Profile/index.jsx
@@ -0,0 +1,20 @@
+const Profile = () => {
+	return (
+		<main className='main'>
+			<div>
+				<img src='https://avatars.mds.yandex.net/i?id=1e1f9124c6bc741cd9b2ba9478b3347e-5289357-images-thumbs&n=13' />
+			</div>
+			<div>ava+desc</div>
+			<div>
+				My posts
+				<div>New post</div>
+				<div>
+					<div>Post 1</div>
+					<div>Post 2</div>
+				</div>
+			</div>
+		</main>
+	);
+};
+
+export default Profile;
